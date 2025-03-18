ec6d9e4ac257920d6fee5daae3cdf541998f2be1
rafaelasantana1 <rafasantosa2004@gmail.com>
2025-03-18 09:14:04 -0300

    Rafaela

---
+++ Rafaela/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace Rafaela.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Rgm { get; set; }
+        public string? Professor { get; set; }
+        public string? Email { get; set; }
+        public DateTime Idade{ get; set; }
+
+    }
+}
